2e082cef00e69f3b75514bb4d5d3be8412c60337
whs <hswongac@gmail.com>
2026-01-07 06:37:07 +0000

    Update

--- nix/local/packages.nix
+++ nix/local/packages.nix
@@ -200,6 +200,21 @@ with inputs.nixpkgs; let
     dontPatchELF = true;
     inherit postFixup;
   };
+  gaiad_25_3_0 = stdenv.mkDerivation rec {
+    pname = "gaiad";
+    version = "25.3.0";
+    src = fetchurl {
+      url = "https://github.com/cosmos/gaia/releases/download/v${version}/gaiad-v${version}-linux-amd64";
+      sha256 = "sha256-OWwvCMbWDMFMEBODoy8yAjeeRNT8SmVQXbs4l+Dx4e0=";
+    };
+    dontUnpack = true;
+    dontBuild = true;
+    dontConfigure = true;
+    dontInstall = true;
+    dontPatch = true;
+    dontPatchELF = true;
+    inherit postFixup;
+  };
 in {
   inherit gaiad_21_0_1;
   inherit gaiad_22_0_2;
@@ -214,4 +229,5 @@ in {
   inherit gaiad_25_0_0;
   inherit gaiad_25_1_0;
   inherit gaiad_25_2_0;
+  inherit gaiad_25_3_0;
 }
